e05187d94a0fe6c880fbc3d822b2ab55988b9ded
LORDMarciXD <43565242+LORDMarciXD@users.noreply.github.com>
2019-04-11 12:07:25 +0200

    Add files via upload

---
+++ Project_Arduino_LCD_KeypadShield_Menu/Project_Arduino_LCD_KeypadShield_Menu.ino
@@ -0,0 +1,334 @@
+// Marcel Izlakar
+
+String menuItems[] = {"  0,2s", "  0,5s", "  1s", "  2s"};
+
+int readKey;
+int savedDistance = 0;
+
+int menuPage = 0;
+int maxMenuPages = round(((sizeof(menuItems) / sizeof(String)) / 2) + .5);
+int cursorPosition = 0;
+
+byte downArrow[8] = {
+  0b00100, //   *
+  0b00100, //   *
+  0b00100, //   *
+  0b00100, //   *
+  0b00100, //   *
+  0b10101, // * * *
+  0b01110, //  ***
+  0b00100  //   *
+};
+ 
+byte upArrow[8] = {
+  0b00100, //   *
+  0b01110, //  ***
+  0b10101, // * * *
+  0b00100, //   *
+  0b00100, //   *
+  0b00100, //   *
+  0b00100, //   *
+  0b00100  //   *
+};
+ 
+byte menuCursor[8] = {
+  B01000, //  *
+  B00100, //   *
+  B00010, //    *
+  B00001, //     *
+  B00010, //    *
+  B00100, //   *
+  B01000, //  *
+  B00000  //
+};
+ 
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+ 
+void setup() {
+
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  lcd.clear();
+
+  lcd.createChar(0, menuCursor);
+  lcd.createChar(1, upArrow);
+  lcd.createChar(2, downArrow);
+ 
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(3,OUTPUT);
+ 
+}
+ 
+void loop() {
+  mainMenuDraw();
+  drawCursor();
+  operateMainMenu();
+}
+
+void mainMenuDraw() {
+  Serial.print(menuPage);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print(menuItems[menuPage]);
+  lcd.setCursor(1, 1);
+  lcd.print(menuItems[menuPage + 1]);
+  if (menuPage == 0) {
+    lcd.setCursor(15, 1);
+    lcd.write(byte(2));
+  } else if (menuPage > 0 and menuPage < maxMenuPages) {
+    lcd.setCursor(15, 1);
+    lcd.write(byte(2));
+    lcd.setCursor(15, 0);
+    lcd.write(byte(1));
+  } else if (menuPage == maxMenuPages) {
+    lcd.setCursor(15, 0);
+    lcd.write(byte(1));
+  }
+}
+
+void drawCursor() {
+  for (int x = 0; x < 2; x++) {
+    lcd.setCursor(0, x);
+    lcd.print(" ");
+  }
+ 
+  if (menuPage % 2 == 0) {
+    if (cursorPosition % 2 == 0) {
+      lcd.setCursor(0, 0);
+      lcd.write(byte(0));
+    }
+    if (cursorPosition % 2 != 0) {
+      lcd.setCursor(0, 1);
+      lcd.write(byte(0));
+    }
+  }
+  if (menuPage % 2 != 0) {
+    if (cursorPosition % 2 == 0) {
+      lcd.setCursor(0, 1);
+      lcd.write(byte(0));
+    }
+    if (cursorPosition % 2 != 0) {
+      lcd.setCursor(0, 0);
+      lcd.write(byte(0));
+    }
+  }
+}
+ 
+ 
+void operateMainMenu() {
+  int activeButton = 0;
+  while (activeButton == 0) {
+    int button;
+    readKey = analogRead(0);
+    if (readKey < 790) {
+      delay(100);
+      readKey = analogRead(0);
+    }
+    button = evaluateButton(readKey);
+    switch (button) {
+      case 0:
+        break;
+      case 1:
+        button = 0;
+        switch (cursorPosition) {
+          case 0:
+            menuItem1();
+            break;
+          case 1:
+            menuItem2();
+            break;
+          case 2:
+            menuItem3();
+            break;
+          case 3:
+            menuItem4();
+            break;
+         
+        }
+        activeButton = 1;
+        mainMenuDraw();
+        drawCursor();
+        break;
+      case 2:
+        button = 0;
+        if (menuPage == 0) {
+          cursorPosition = cursorPosition - 1;
+          cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+        }
+        if (menuPage % 2 == 0 and cursorPosition % 2 == 0) {
+          menuPage = menuPage - 1;
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+ 
+        if (menuPage % 2 != 0 and cursorPosition % 2 != 0) {
+          menuPage = menuPage - 1;
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+ 
+        cursorPosition = cursorPosition - 1;
+        cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+ 
+        mainMenuDraw();
+        drawCursor();
+        activeButton = 1;
+        break;
+      case 3:
+        button = 0;
+        if (menuPage % 2 == 0 and cursorPosition % 2 != 0) {
+          menuPage = menuPage + 1;
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+ 
+        if (menuPage % 2 != 0 and cursorPosition % 2 == 0) {
+          menuPage = menuPage + 1;
+          menuPage = constrain(menuPage, 0, maxMenuPages);
+        }
+ 
+        cursorPosition = cursorPosition + 1;
+        cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+        mainMenuDraw();
+        drawCursor();
+        activeButton = 1;
+        break;
+    }
+  }
+}
+
+int evaluateButton(int x) {
+  int result = 0;
+  if (x < 50) {
+    result = 1;
+  } else if (x < 195) {
+    result = 2;
+  } else if (x < 380) {
+    result = 3;
+  } else if (x < 790) {
+    result = 4;
+  }
+  return result;
+}
+
+void drawInstructions() {
+  lcd.setCursor(0, 1);
+  lcd.print("Use ");
+  lcd.write(byte(1));
+  lcd.print("/");
+  lcd.write(byte(2));
+  lcd.print(" buttons");
+}
+ 
+void menuItem1() {
+  int activeButton = 0;
+ 
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Utripa 0,2s");
+ 
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+ 
+  while (activeButton == 0) {
+    int button;
+    readKey = analogRead(0);
+    if (readKey < 790) {
+      delay(100);
+      readKey = analogRead(0);
+    }
+    button = evaluateButton(readKey);
+    switch (button) {
+      case 4:
+        button = 0;
+        activeButton = 1;
+        digitalWrite(13,LOW);
+        break;
+    }
+  }
+}
+ 
+void menuItem2() {
+  int activeButton = 0;
+ 
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Utripa 0,5s");
+ 
+  digitalWrite(12,HIGH);
+ 
+  while (activeButton == 0) {
+    int button;
+    readKey = analogRead(0);
+    if (readKey < 790) {
+      delay(100);
+      readKey = analogRead(0);
+    }
+    button = evaluateButton(readKey);
+    switch (button) {
+      case 4:
+        button = 0;
+        activeButton = 1;
+        digitalWrite(12,LOW);
+        break;
+    }
+  }
+}
+ 
+void menuItem3() {
+  int activeButton = 0;
+ 
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Utrip 1s");
+ 
+  digitalWrite(11,HIGH);
+ 
+  while (activeButton == 0) {
+    int button;
+    readKey = analogRead(0);
+    if (readKey < 790) {
+      delay(100);
+      readKey = analogRead(0);
+    }
+    button = evaluateButton(readKey);
+    switch (button) {
+      case 4:
+        button = 0;
+        activeButton = 1;
+        digitalWrite(11,LOW);
+        break;
+    }
+  }
+}
+ 
+void menuItem4() {
+  int activeButton = 0;
+ 
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Utripa 4s");
+ 
+  digitalWrite(3,HIGH);
+ 
+  while (activeButton == 0) {
+    int button;
+    readKey = analogRead(0);
+    if (readKey < 790) {
+      delay(100);
+      readKey = analogRead(0);
+    }
+    button = evaluateButton(readKey);
+    switch (button) {
+      case 4:
+        button = 0;
+        activeButton = 1;
+        digitalWrite(3,LOW);
+        break;
+    }
+  }
+}
